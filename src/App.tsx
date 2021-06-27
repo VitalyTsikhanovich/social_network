bd7d137c2fa8f2614a6d67115e4aba1b656eae0a
tihon <tihonnet@gmail.com>
2021-06-27 18:24:48 +0300

    feat: add WithSuspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React from "react";
-// import logo from './logo.svg';
+import React ,{ Suspense } from "react";
 import './App.css'
 import Navbar from "./components/Navbar/Navbar";
 import Music from "./components/Music/Music"
 import { Switch, Route} from "react-router-dom";
 import News from "./components/News/News";
 import Setting from "./components/Setting/Setting";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {connect} from "react-redux";
@@ -16,6 +13,12 @@ import {getAuthUserData} from "./redux/auth-reducer";
 import {initializeApp} from "./redux/app-reducer";
 import {AppStateType} from "./redux/redux-store";
 import Preloader from "./components/common/Preloader/ Preloader";
+
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// import ProfileContainer from "./components/Profile/ProfileContainer";
+
 
 type AppPropsType = {
     // posts: Array<PostsType>
@@ -47,8 +50,16 @@ class App extends React.Component<AppPropsType> {
 
                         <div className={'app-wrapper-content'}>
                             <Switch>
-                                <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                                <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                                <Route path='/profile/:userId?' render={() => {
+                                    return <Suspense fallback={<Preloader/>} >
+                                     <ProfileContainer/>
+                                 </Suspense>
+                                }}/>
+                                <Route path='/dialogs' render={() => {
+                                  return <Suspense fallback={<Preloader/>}>
+                                    <DialogsContainer/>
+                                  </Suspense>
+                                }}/>
                                 <Route path='/music' render={() => <Music/>}/>
                                 <Route path='/news' render={() => <News/>}/>
                                 <Route path='/setting' render={() => <Setting/>}/>
---
+++ src/hoc/WithSuspense.tsx
@@ -0,0 +1,14 @@
+import React, {ComponentType, Suspense} from "react";
+import Preloader from "../components/common/Preloader/ Preloader";
+
+
+export function withSuspense<T>(Component: ComponentType<T>) {
+
+    return (props: T) => {
+        return <Suspense fallback={<Preloader/>}>
+            <Component {...props}/>
+        </Suspense>
+
+    }
+}
+
